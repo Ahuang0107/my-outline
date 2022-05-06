c1c2657465337ee13c31682b4cb673befb292aff
Elase Huang <elase.huang@outlook.com>
2022-05-07 02:18:03 +0800

    initial to hello world

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,29 +1,13 @@
 import * as React from "react";
 import { render } from "react-dom";
-import "./style.css";
-import "./style.scss";
-import "./style.less";
-import "./font.css";
-import { Provider } from "react-redux";
-import { Counter, CssImageTest, JsImageTest } from "./component";
-import { store } from "./store";
 
-// test comments
-// eslint-disable-next-line no-console
-console.log("start");
+const element = window.document.getElementById("react-page");
 
-render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <div className="custom-div">
-        <h1 className="custom-h1">welcome</h1>
-      </div>
-      <h1 className="custom-h1">welcome</h1>
-      <p className="custom-p">你好，世界</p>
-      <CssImageTest />
-      <JsImageTest />
-      <Counter />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("react-page")
-);
+if (element) {
+  const App = () => (
+    <React.StrictMode>
+      <h1>Hello World!</h1>
+    </React.StrictMode>
+  );
+  render(<App />, element);
+}

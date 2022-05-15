dd4631d4936c855968f8c6f63284fc368dc4830b
Elase Huang <elase.huang@outlook.com>
2022-05-16 00:53:59 +0800

    use Inter as default font family

--- src/demo/styles/globals.ts
+++ src/demo/styles/globals.ts
@@ -21,7 +21,6 @@ export default createGlobalStyle`
   optgroup,
   select,
   textarea {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Inter, sans-serif;
   }
 `;

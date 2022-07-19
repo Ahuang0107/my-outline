9220cb7f178b5e4f8aa96d1887294489eaaa87cc
Elase Huang <elase.huang@outlook.com>
2022-07-19 14:48:33 +0800

    chore: Document Model 增加 findByPK 方法

--- src/server/models/Document.ts
+++ src/server/models/Document.ts
@@ -1,10 +1,20 @@
 import { Column, DataType, Table } from "sequelize-typescript";
+import { FindOptions } from "sequelize";
 import ParanoidModel from "./base/ParanoidModel";
 
 @Table({ tableName: "documents", modelName: "document" })
 class Document extends ParanoidModel {
   @Column(DataType.STRING)
   title: string;
+
+  static async findByPk(id: string, options?: FindOptions<Document>) {
+    return this.findOne({
+      where: {
+        id,
+      },
+      ...options,
+    });
+  }
 }
 
 export default Document;
